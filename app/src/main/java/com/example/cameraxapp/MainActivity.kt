7f1e3062d767cfac21af83ec0068bcc44857bb01
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-07-13 15:55:39 +0700

    requesting camera permission

--- app/src/main/java/com/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.cameraxapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.cameraxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        requestingPermissions()
+    }
+
+    private fun requestingPermissions() {
+        if (permissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION)
+        }
+    }
+
+    private fun permissionsGranted(): Boolean = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSION && permissionsGranted()) {
+            startCamera()
+        } else {
+            Toast.makeText(this, "camera permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startCamera() {
+        Toast.makeText(this, "camera permission granted", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSION = 101
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
     }
 }
